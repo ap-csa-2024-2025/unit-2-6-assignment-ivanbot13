de1b0e6dbc903ea5aef18dac53a2722ee4ee1847
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-10-07 10:55:18 -0700

    come back later

--- Main.java
+++ Main.java
@@ -5,5 +5,10 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    //prob 1:
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Radius: ");
+    double radius = sc.nextDouble();
+
   }
 }

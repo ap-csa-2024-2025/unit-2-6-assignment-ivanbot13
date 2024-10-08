8726b7fc11139d4cf489d4e9af0c2249be523a55
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-10-08 11:11:34 -0700

    prob 1

--- Main.java
+++ Main.java
@@ -4,11 +4,16 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
-    //prob 1:
+    // prob 1:
     Scanner sc = new Scanner(System.in);
     System.out.println("Radius: ");
     double radius = sc.nextDouble();
+    Circle circle = new Circle(radius);
+    double circum = circle.getCircumference();
+    double area = circle.getArea();
+    System.out.println("A circle with a radius of " + radius + " has a circumference of " + circum + " and an area of " + area);
+
+    // prob 2:
 
   }
 }

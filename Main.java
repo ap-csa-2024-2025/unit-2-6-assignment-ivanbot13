4cdb6442282a439f5927d9eb7225f29f34605b91
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-10-08 11:20:02 -0700

    2error

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // prob 1:
     Scanner sc = new Scanner(System.in);
+    // prob 1:
     System.out.println("Radius: ");
     double radius = sc.nextDouble();
     Circle circle = new Circle(radius);
@@ -14,6 +14,16 @@ public class Main
     System.out.println("A circle with a radius of " + radius + " has a circumference of " + circum + " and an area of " + area);
 
     // prob 2:
-
+    System.out.println("Number of sides: ");
+    double sides = sc.nextDouble();
+    System.out.println("Side length: ");
+    double length = sc.nextDouble();
+    RegularPolygon shape = new RegularPolygon(sides, length);
+    area = shape.getArea();
+    System.out.println("Area with " + sides + " sides: " + area);
+    shape = shape.addSides(2); //error
+    System.out.println("Incrementing the number of sides by two");
+    area = shape.getArea();
+    System.out.println("Area with " + (sides + 2) + " sides: " + area);
   }
 }
